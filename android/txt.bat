148e6bbd08ce93f95547012633e6f5eb597a50d2
hai061898 <truonghaiauto98@gmail.com>
2021-10-01 16:01:46 +0700

    login signup

---
+++ android/txt.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jre1.8.0_301\bin\keytool" -list -v -keystore "D:\Flutter\Awquiz\quiz\android\key.keystore" -alias alias_name
+pause
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz/pages/homescreen.dart';
 import 'package:quiz/pages/onboarding.dart';
 
 void main() {
@@ -6,17 +9,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const OnboardingPage(),
-    );
+        title: 'Quiz',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: FutureBuilder(
+          future: Firebase.initializeApp(),
+          builder: (context, snaps) {
+            if (!snaps.hasData) return const OnboardingPage();
+            return StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return const OnboardingPage();
+                return const HomeScreen();
+              },
+            );
+          },
+        ));
   }
 }
